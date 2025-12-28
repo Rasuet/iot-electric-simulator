3937ae569e582fe785a3b33a7e50d9be46144781
Rasuet <rasuet@gmail.com>
2025-12-28 13:59:51 +0100

    Versión final funcional de la lectura del contador

--- src/main/java/com/antonio/iot/ContadorController.java
+++ src/main/java/com/antonio/iot/ContadorController.java
@@ -1,4 +1,31 @@
 package com.antonio.iot;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class ContadorController {
+
+    public void leerDatosDispositivo(String nombreArchivo) {
+        try {
+            // La barra "/" al principio es clave: busca en la raíz de 'resources'
+            InputStream is = getClass().getResourceAsStream("/" + nombreArchivo);
+
+            if (is == null) {
+                System.out.println("¡Error! No encuentro el archivo: " + nombreArchivo);
+                return;
+            }
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String linea;
+            System.out.println("--- Iniciando Lectura del Contador ---");
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error: " + e.getMessage());
+        }
+    }
 }
